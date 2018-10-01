f872e6f3d0e69a831b1943b25d1540792e8d9661
Dave Serrée <388343@student.fontys.nl>
2018-10-01 15:38:03 +0200

    test

--- Interface/Server_Interface/Server_Interface/Form1.cs
+++ Interface/Server_Interface/Server_Interface/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace Server_Interface
 {
     public partial class Form1 : Form
     {
+        private TcpClient interfaceClient;
+        private int port;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Connect_btn_Click(object sender, EventArgs e)
+        {
+            interfaceClient = new TcpClient();
+        }
+
+        private void Disconnect_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendData_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GetData_btn_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
