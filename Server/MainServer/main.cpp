53045670094482fcc1a6b27ea6e60514b753d5f7
Smeele,Nick N <n.smeele@student.fontys.nl>
2018-10-01 11:00:40 +0200

    Reverted threads

--- Server/MainServer/main.cpp
+++ Server/MainServer/main.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
 #include <vector>
+//#include <thread> 
 
 #include "client.h"
 #include "socket.h"
 
+//Client == Interface (C#)
+//Socket = machines (C++)
+
+bool quit;
+
+        
+static void HandleUserInput()
+{
+    while(!quit)
+    {
+        std::string commando;
+        std::cin >> commando;
+        std::cin.ignore();
+        
+        //string[] commando = Console.ReadLine().ToLower().Split(' ');
+
+        try
+        {
+            if ((commando == "exit") || (commando == "quit"))
+            {
+                quit = true;
+            }
+            else
+            {
+                std::cout << "Invalid arg: " << commando;
+            }
+        }
+        catch(std::exception)
+        {
+            std::cout << "Exception: " << commando;
+        }
+    }
+}
 
 int main( void )
 {  
-    std::cout << "------------------\n  Server started\n------------------\n";
+    std::cout << "------------------\n  Setting up Server\n------------------\n";
+    quit = false;
+    //Client* client = new Client();
     Socket* socket = new Socket();
-
     socket->Send();
+    //std::thread inputThread (HandleUserInput); 
+    std::cout << "------------------\n  Server started\n------------------\n";
+
+    while(!quit){
+        HandleUserInput();
+
+    }
+
     std::cout << "\nServer Stopped...\n\n";
     
     return 0;
