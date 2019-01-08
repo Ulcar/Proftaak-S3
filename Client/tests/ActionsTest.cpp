ac1c5591e3e61dcdea212bc44a4d5c2eb72d27c2
Erik Verstegen <erik@flowingbytes.nl>
2019-01-08 15:23:44 +0100

    Add the mock class of the ICentipedeShield and test the Buzzer action

--- Client/tests/ActionsTest.cpp
+++ Client/tests/ActionsTest.cpp
@@ -2,14 +2,49 @@
 #include <gmock/gmock.h>
 
 #include "harnass/ArduinoWrapper.h"
+#include "mock/MCentipedeShield.h"
 
+#include "HardwareControl.ino"
 #include "Actions.ino"
+#include "Controls.ino"
+#include "Heater.ino"
+#include "Motor.ino"
+#include "Water.ino"
 
+using ::testing::NiceMock;
 using ::testing::AllOf;
+using ::testing::Test;
 using ::testing::Ge;
 using ::testing::Le;
+using ::testing::_;
 
-TEST(ActionsTest, TestDelayTakesTheCorrectTimeToBeDone)
+class ActionTest : public ::testing::Test
+{
+protected:
+    NiceMock<MCentipedeShield> centipede;
+
+    HardwareControl* control;
+    IAction* action;
+
+    ActionTest()
+    {
+        // These are all of the Initialize calls on the hardware components.
+        EXPECT_CALL(centipede, DigitalWrite(_, _)).Times(7 + 3 + 1 + 3 + 2);
+
+        control = new HardwareControl(&centipede, new Controls(), new Heater(), new Motor(), new Water());
+    }
+
+    virtual ~ActionTest() { };
+
+    void SetAction(IAction* action)
+    {
+        action->SetHardwareControl(control);
+
+        this->action = action;
+    }
+};
+
+TEST(DelayActionTest, TestDelayTakesTheCorrectTimeToBeDone)
 {
     unsigned long runTime = 1000L;
     unsigned long margin = 5L;
@@ -31,7 +66,11 @@ TEST(ActionsTest, TestDelayTakesTheCorrectTimeToBeDone)
     EXPECT_THAT(time, AllOf(Ge(runTime - margin), Le(runTime + margin)));
 }
 
-TEST(ActionsTest, TestBuzzerCorrectlyGetsSet)
+TEST_F(ActionTest, TestBuzzerCorrectlyGetsSet)
 {
-    BuzzerAction action = BuzzerAction(STATE_ON);
+    EXPECT_CALL(centipede, DigitalWrite(OUTPUT_BUZZER, HIGH)).Times(1);
+
+    SetAction(new BuzzerAction(STATE_ON));
+
+    action->Handle();
 }
